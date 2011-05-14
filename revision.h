4f8c06625d6cc43945a5f056d67b0c6f29d41a37
philipp.dallig@web.de <philipp.dallig@web.de@e5ed2614-5e12-5c95-e581-09b7740504a2>
2011-05-14 16:01:05 +0000

    merge with rsa scripts

--- revision.h
+++ revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "103 at /dev/rsa branch"
- #define REVISION_DATE "2011-04-12"
- #define REVISION_TIME "20:37:11"
+ #define REVISION_ID "106 at /dev/rsa branch"
+ #define REVISION_DATE "2011-04-30"
+ #define REVISION_TIME "16:16:36"
 #endif // __REVISION_H__
